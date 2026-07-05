9abf7c7c7930bdad390bf4d6ddeb2b2978971608
Ravitejakolaparthi <ravitejakolaparthi97@gmail.com>
2026-07-05 09:26:41 +0530

    ✅ Balanced Binary Tree | Easy | C++ | Runtime: 0 ms | Memory: 8.1 MB | 2026-07-05

---
+++ leetcode/easy/0110-balanced-binary-tree/solution.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isBalanced(TreeNode* root) {
+        if(root == nullptr)
+        return 0;
+        else
+        {
+            int h = isBalanced(root->left) - isBalanced(root->right);
+            if(h<= -1 && h>= 1)
+            return false;
+            else
+            return max(isBalanced(root->left),isBalanced(root->right))+1;
+            
+        }
+    }
+};
