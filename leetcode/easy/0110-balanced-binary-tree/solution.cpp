dcc75bb4e78640a03a7ac0b86bf7a0207d1c6f85
Ravitejakolaparthi <ravitejakolaparthi97@gmail.com>
2026-07-05 11:20:42 +0530

    ✅ Balanced Binary Tree | Easy | C++ | Runtime: 0 ms | Memory: 8.2 MB | 2026-07-05

--- leetcode/easy/0110-balanced-binary-tree/solution.cpp
+++ leetcode/easy/0110-balanced-binary-tree/solution.cpp
@@ -11,27 +11,25 @@
  */
 class Solution {
 public:
-    int check(TreeNode* root)
+    int check(TreeNode * root)
     {
-        int flag = 0;
-        int lh = 0;
-        int rh = 0;
-        if(root == nullptr)
-        return 0;
-        else
-        {
-            lh = check(root -> left) +1;
-            rh = check(root -> right) +1;
-            if(lh-rh <= -1 && lh -rh >=1)
-            {
-                flag = 1;
+            int flag = 0;
+            int lh = 0;
+            int rh = 0;
+            if(root == nullptr)
+            return 0;
+            else{
+                lh = check(root -> left);
+                rh = check(root -> right);
+                if(lh <= -1 || rh >= -1 || abs(lh - rh) > 1)
+                {
+                    flag = 1;
+                }
             }
-        }
-        if(flag == 1)
-        return 0;
-        else
-        return 1;
-
+            if(flag == 1)
+            return -1;
+            else
+            return max(lh,rh)+1;
     }
     bool isBalanced(TreeNode* root) {
       return check(root);
