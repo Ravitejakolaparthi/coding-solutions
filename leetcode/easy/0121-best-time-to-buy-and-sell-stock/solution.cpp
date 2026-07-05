5c8a4d68b42bff51bf37b87685defa2746fb2cfb
Ravitejakolaparthi <ravitejakolaparthi97@gmail.com>
2026-07-05 12:52:10 +0530

    ✅ Best Time to Buy and Sell Stock | Easy | C++ | Runtime: 5 ms (8.28%) | Memory: 101.2 MB | 2026-07-05

--- leetcode/easy/0121-best-time-to-buy-and-sell-stock/solution.cpp
+++ leetcode/easy/0121-best-time-to-buy-and-sell-stock/solution.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-       vector<int> bbuy(prices.size());
-       bbuy[0] = prices[0];
+       vector<int> bsell(prices.size());
+       bsell[0] = prices[0];
        for(int i = 1;i<prices.size();i++)
        {
-            bbuy[i] = min(bbuy[i-1],prices[i]);
+            bsell[i] = min(bsell[i-1],prices[i]);
        }
       int sum = 0;
-      for(int i = 0;i<bbuy.size();i++)
+      for(int i = 0;i<bsell.size();i++)
       {
-         sum = max(sum,abs(prices[i]-bbuy[i]));
+         sum = max(sum,abs(prices[i]-bsell[i]));
       }
       
        return sum;
