e6c9f544283fd30cd4cda42ace2d1dbfdecac064
Ravitejakolaparthi <ravitejakolaparthi97@gmail.com>
2026-07-05 08:15:44 +0530

    ✅ Q1. Sum of Integers with Maximum Digit Range | Easy | C++ | Runtime: 0 ms (100.00%) | Memory: 31.8 MB | 2026-07-05

---
+++ leetcode/easy/4356-sum-of-integers-with-maximum-digit-range/solution.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int rangeof(int n)
+    {
+        vector<int>v;
+         int s = 999999;
+         int l = -1;
+         int rem = 0;
+         while(n>0)
+             {
+                rem = n%10;
+                s = min(s,rem);
+                 // cout << s <<endl;
+                l = max(l,rem);
+                 // cout << l <<endl;
+                n = n/10; 
+             }
+        int r = l-s;
+        return r;
+       
+    }
+    
+    int maxDigitRange(vector<int>& nums) {
+        vector<int>v(nums.size());
+        int m = -1;
+        for(int i = 0;i<nums.size();i++)
+            {
+                v[i] = rangeof(nums[i]);  
+                m = max(m,v[i]);
+            }
+            int sum = 0;
+            for(int i = 0;i<nums.size();i++)
+                {
+                    if(v[i] == m)
+                    {
+                        sum+=nums[i];
+                    }
+                }
+           
+        
+        return sum;
+    }
+};
