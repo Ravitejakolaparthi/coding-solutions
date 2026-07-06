d1bb8c08b6a44d2ac60006ef8a94f479caeae552
Ravitejakolaparthi <ravitejakolaparthi97@gmail.com>
2026-07-06 15:12:46 +0530

    ✅ Reverse Bits | Easy | C++ | Runtime: 2 ms | Memory: 7.9 MB | 2026-07-06

---
+++ leetcode/easy/0190-reverse-bits/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int reverseBits(int n) {
+        int r =0;
+        int b =0;
+
+        for(int i = 0;i<32;i++)
+        {
+                r << 1;
+                b = n&1;
+                r |=b;
+                r >> 1;
+        }
+        return r;
+    }
+};
