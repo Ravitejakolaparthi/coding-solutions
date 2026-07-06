7db7c8e6da2dd278400f0f5c65ac81952dd37722
Ravitejakolaparthi <ravitejakolaparthi97@gmail.com>
2026-07-06 10:06:02 +0530

    ✅ Single Number | Easy | C++ | Runtime: 0 ms | Memory: 8.3 MB | 2026-07-06

--- leetcode/easy/0136-single-number/solution.cpp
+++ leetcode/easy/0136-single-number/solution.cpp
@@ -4,12 +4,15 @@ public:
         int n = nums[0];
         int f = 0;
         sort(nums.begin(),nums.end());
-        for(int i = 1;i<nums.size();i+=2)
+        for(int i = 1;i<nums.size()-1;i+=2)
         {
               if(nums[i-1]^nums[i] != 0)
               {
-                f = 1;
-                 n = nums[i];
+                 if(i != nums.size())
+                 {
+                 f = 1;
+                 n = nums[i-1];
+                 }
               }
              
         }
