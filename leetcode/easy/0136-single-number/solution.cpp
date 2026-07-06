c13325bd5fbaada372a23f007a8cea54bfeb346c
Ravitejakolaparthi <ravitejakolaparthi97@gmail.com>
2026-07-06 15:34:34 +0530

    ✅ Single Number | Easy | C++ | Runtime: 0 ms (100.00%) | Memory: 20.8 MB | 2026-07-06

--- leetcode/easy/0136-single-number/solution.cpp
+++ leetcode/easy/0136-single-number/solution.cpp
@@ -2,6 +2,19 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         int n = 0;
+        /*
+        XOR cancels same numbers like 
+        uses associative and commitative property
+        ex -> 4 1 2 1 2
+        here
+        n = 4
+        n = 4^1 diff so nothin happen
+        n = 4^1^2    ""
+        n = 4^1^2^1 here according to associative cancel takes place
+        n = 4^2  
+        n = 4^2^2  same cancle takes place
+        n = 4
+        */
         for(int i =0;i<nums.size();i++)
         {
             n^=nums[i];
