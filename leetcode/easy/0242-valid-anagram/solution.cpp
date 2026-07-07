80d592af1a4fce3b8a440f5604099ba373f6b056
Ravitejakolaparthi <ravitejakolaparthi97@gmail.com>
2026-07-07 14:44:03 +0530

    ✅ Valid Anagram | Easy | C++ | Runtime: 0 ms | Memory: 7.6 MB | 2026-07-07

--- leetcode/easy/0242-valid-anagram/solution.cpp
+++ leetcode/easy/0242-valid-anagram/solution.cpp
@@ -5,6 +5,10 @@ public:
         for(int i = 0;i<s.size();i++)
         {
             m^= s[i];
+           
+        }
+        for(int i = 0;i<t.size();i++)
+        {
             m^=t[i];
         }
         if(m == 0)
