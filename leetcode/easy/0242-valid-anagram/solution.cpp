552d48f9b8ea1882ab5e63ee8c76c9f0e50626da
Ravitejakolaparthi <ravitejakolaparthi97@gmail.com>
2026-07-09 06:17:24 +0530

    ✅ Valid Anagram | Easy | C++ | Runtime: 0 ms | Memory: 7.9 MB | 2026-07-09

--- leetcode/easy/0242-valid-anagram/solution.cpp
+++ leetcode/easy/0242-valid-anagram/solution.cpp
@@ -3,6 +3,8 @@ public:
     bool isAnagram(string s, string t) {
             sort(s.begin(),s.end());
             sort(t.begin(),t.end());
+            if(s.size()!=t.size())
+            return false;
             for(int i = 0;i<s.size();i++)
             {
                 if(s[i]!=t[i])
