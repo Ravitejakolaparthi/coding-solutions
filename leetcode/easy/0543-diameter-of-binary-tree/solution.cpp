2ccc4a7bfb412aecd08911ddfeaff7f5567c60c4
Ravitejakolaparthi <ravitejakolaparthi97@gmail.com>
2026-07-10 15:18:08 +0530

    ✅ Diameter of Binary Tree | Easy | C++ | Runtime: 339 ms (5.09%) | Memory: 23.7 MB | 2026-07-10

---
+++ leetcode/easy/0543-diameter-of-binary-tree/solution.cpp
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int depthofleft(TreeNode*root)
+    {
+        if(root == nullptr)
+        {
+            return 0;
+        }
+        else
+        {
+            return max(depthofleft(root->left),depthofleft(root->right))+1;
+        }
+    }
+     int depthofright(TreeNode*root)
+    {
+        if(root == nullptr)
+        {
+            return 0;
+        }
+        else
+        {
+            return max(depthofright(root->left),depthofright(root->right))+1;
+        }
+    }
+    int Traverse(TreeNode*root,int m){
+        if(root != nullptr)
+        {       m = Traverse(root -> left,m);
+                int a =   depthofleft(root->left);
+                int b =   depthofright(root->right);
+                m = max(m,a + b);
+                m = Traverse(root->right,m);
+        }
+        return m;
+    }
+    int diameterOfBinaryTree(TreeNode* root) {
+    int c = 0; 
+     c = Traverse(root,0); 
+     return c;
+    }
+};
