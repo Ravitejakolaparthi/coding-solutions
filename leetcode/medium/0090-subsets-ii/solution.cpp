2573d23461a6d7d5f3c4c2897079559064904da2
Ravitejakolaparthi <ravitejakolaparthi97@gmail.com>
2026-07-13 07:25:24 +0530

    ✅ Subsets II | Medium | C++ | Runtime: 0 ms | Memory: 9.7 MB | 2026-07-13

---
+++ leetcode/medium/0090-subsets-ii/solution.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>>b;
+    void solve(vector<int>&n,vector<int>v,int i)
+    {
+        if(i == n.size())
+        {
+            b.push_back(v);
+        }
+        else
+        {
+            solve(n,v,i+1);
+            v.push_back(n[i]);
+            solve(n,v,i+1);
+        }
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+       vector<int>u;
+       solve(nums,u,0);
+       set<vector<int>>s(b.begin(),b.end());
+       vector<vector<int>>v(s.begin(),s.end()); 
+       return v;
+    }
+};
