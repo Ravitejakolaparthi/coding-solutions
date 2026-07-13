2949ebfa116bc9099b0ece4f02f3e52e54bfb474
Ravitejakolaparthi <ravitejakolaparthi97@gmail.com>
2026-07-13 07:09:58 +0530

    ✅ Subsets | Medium | C++ | Runtime: 3 ms (27.77%) | Memory: 15.9 MB | 2026-07-13

---
+++ leetcode/medium/0078-subsets/solution.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<int>>b;
+    void solve(vector<int>&n,vector<int>v,int i)
+    {
+        if(i == n.size())
+        {
+            b.push_back(v);
+           
+        }
+        else
+        {
+            v.push_back(n[i]);
+            solve(n,v,i+1);
+            v.pop_back();
+            solve(n,v,i+1);
+        }
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<int>v;
+        solve(nums,v,0);
+        return b;
+        
+    }
+};
