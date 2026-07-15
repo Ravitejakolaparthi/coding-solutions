311baf6de9d69ce453806cd4549bf1c60aa8bc91
Ravitejakolaparthi <ravitejakolaparthi97@gmail.com>
2026-07-15 06:46:37 +0530

    ✅ Letter Case Permutation | Medium | C++ | Runtime: 1 ms (67.19%) | Memory: 15.8 MB | 2026-07-15

---
+++ leetcode/medium/0784-letter-case-permutation/solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<string>a;
+    void solve(string v,int i)
+    {
+        if(i == v.size())
+        {
+            a.push_back(v);
+        }
+        else
+        {
+            if(isalpha(v[i]))
+            {
+                v[i] = tolower(v[i]);
+                solve(v,i+1);
+
+                v[i] = toupper(v[i]);
+            }
+            solve(v,i+1);
+        }
+    }
+    vector<string> letterCasePermutation(string s) {
+            solve(s,0);
+            return a;
+    }
+};
