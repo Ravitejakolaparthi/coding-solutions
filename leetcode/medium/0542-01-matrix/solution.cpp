46bcd02317f11c028875d4dd3b793d2a3fc28f0d
Ravitejakolaparthi <ravitejakolaparthi97@gmail.com>
2026-07-21 21:48:35 +0530

    ✅ 01 Matrix | Medium | C++ | Runtime: 0 ms | Memory: 8.7 MB | 2026-07-21

---
+++ leetcode/medium/0542-01-matrix/solution.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int dfs(vector<vector<int>>&mat,int i,int j,int n,int m,int c,vector<vector<bool>>&visited)
+    {
+        if(i >= 0 && i <=n-1 && j >=0 && j<=m-1)
+        {
+            if(mat[i][j] == 0)
+            {
+                return c;
+            }
+            if(visited[i][j] == true)
+            {
+                return c;
+            }
+            else
+            {
+                c+= 1;
+                visited[i][j] =true;
+                c =dfs(mat,i+1,j,n,m,c,visited);
+                c =dfs(mat,i,j+1,n,m,c,visited);
+                c =dfs(mat,i-1,j,n,m,c,visited);
+                c =dfs(mat,i,j-1,n,m,c,visited);
+                visited[i][j] =false;
+            }
+        }
+        return c;
+    }
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int n = mat.size();
+        int m = mat[0].size();
+        int c = 0;
+        vector<vector<int>>v(n,vector<int>(m,0));
+        vector<vector<bool>>visited(n,vector<bool>(m,false));
+        for(int i = 0;i<n;i++)
+        {
+            for(int j = 0;j<m;j++)
+            {
+                if(mat[i][j] == 1 && visited[i][j] == false)
+                {
+                    v[i][j] = dfs(mat,i,j,n,m,c,visited);
+                }
+            }
+        }
+        return v;
+    }
+};
