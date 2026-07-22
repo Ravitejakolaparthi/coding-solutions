4b47023c9613beebe844b79a694662a583369f5b
Ravitejakolaparthi <ravitejakolaparthi97@gmail.com>
2026-07-22 22:11:39 +0530

    ✅ Path with Maximum Gold | Medium | C++ | Runtime: 27 ms | Memory: 27 MB | 2026-07-22

--- leetcode/medium/1219-path-with-maximum-gold/solution.cpp
+++ leetcode/medium/1219-path-with-maximum-gold/solution.cpp
@@ -2,7 +2,8 @@ class Solution {
 public:
     int x =0;
     // vector<int>dx = {0,0,1,-1};
-    // vector<int>dy = {1,-1,0,0};
+    // vector<int>dy = {1,-1,0,0};1
+    vector<int>z;
     int dfs(vector<vector<int>>&grid,vector<vector<bool>>vis,int i,int j,int n,int m,int &cnt)
     {
         if(i>=0 && i<=n-1 && j <= m-1 && j >= 0)
@@ -15,17 +16,16 @@ public:
             else
             {
                 cnt+=grid[i][j];
-                cout << cnt << endl;
-                cout << i << " " << j << endl;
+                // cout << cnt << endl;
+                // cout << i << " " << j << endl;
                 vis[i][j] = true;
                 
-                    x = dfs(grid,vis,i+1,j,n,m,cnt);
-                    x = dfs(grid,vis,i,j+1,n,m,cnt);
-                    x = dfs(grid,vis,i-1,j,n,m,cnt);
-                    x = dfs(grid,vis,i,j+1,n,m,cnt);
-                    x = max(cnt,x);
-                    // cout << x <<"\n";
-
+                     dfs(grid,vis,i+1,j,n,m,cnt);
+                     dfs(grid,vis,i,j+1,n,m,cnt);
+                     dfs(grid,vis,i-1,j,n,m,cnt);
+                     dfs(grid,vis,i,j+1,n,m,cnt);
+                     z.push_back(cnt);
+                     cnt -= grid[i][j];
             }
            
         }
@@ -43,10 +43,13 @@ public:
             {
                 if(grid[i][j] != 0)
                 {
-                   x = max(x,dfs(grid,vis,i,j,n,m,cnt));
+                   dfs(grid,vis,i,j,n,m,cnt);
                 }
             }
         }
-        return cnt;
+        for(auto x : z) cout << x << " ";
+        int ms = 0;
+        ms  = *max_element(z.begin(),z.end());
+        return ms;
     }
 };
