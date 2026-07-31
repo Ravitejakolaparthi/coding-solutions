a092654dbb68336ab21b89c5a532ac36a8d44503
Ravitejakolaparthi <ravitejakolaparthi97@gmail.com>
2026-07-31 22:57:03 +0530

    ✅ Combination Sum II | Medium | C++ | Runtime: 0 ms | Memory: 9.3 MB | 2026-07-31

--- leetcode/medium/0040-combination-sum-ii/solution.cpp
+++ leetcode/medium/0040-combination-sum-ii/solution.cpp
@@ -3,18 +3,22 @@ public:
     vector<vector<int>>b;
     void move(vector<int>&n,int s,vector<int>v,int i)
     {
-        if(s >= 0 && i < n.size())
+        if(s >= 0 && i <= n.size()-1)
         {
             if(s == 0)
             {
-                
+                // sort(v.begin(),v.end());
                 b.push_back(v);
             }
             else
             {
-                move(n,s,v,i+1);
+               
                 v.push_back(n[i]);
                 move(n,s-n[i],v,i+1);
+                v.pop_back();
+                while(i+1 < n.size() &&n[i] == n[i+1])
+                i++;
+                move(n,s,v,i+1);
             }
         }
     }
@@ -22,9 +26,10 @@ public:
      vector<int>v;
      sort(candidates.begin(),candidates.end());
      move(candidates,target,v,0);
-     set<vector<int>>s(b.begin(),b.end());
-     vector<vector<int>>res(s.begin(),s.end());
-     return res;
+    //  set<vector<int>>s(b.begin(),b.end());
+    //  vector<vector<int>>res(s.begin(),s.end());
+
+     return b;
 
     }
 };
