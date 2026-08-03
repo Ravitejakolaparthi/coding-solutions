b78f3a50c7360fa35d21dbba3dd25d492e5de4d5
Ravitejakolaparthi <ravitejakolaparthi97@gmail.com>
2026-08-03 15:24:03 +0530

    ✅ Longest Subarray of 1's After Deleting One Element | Medium | C++ | Runtime: 11 ms (6.18%) | Memory: 60.1 MB | 2026-08-03

--- leetcode/medium/1493-longest-subarray-of-1s-after-deleting-one-element/solution.cpp
+++ leetcode/medium/1493-longest-subarray-of-1s-after-deleting-one-element/solution.cpp
@@ -1,38 +1,19 @@
 class Solution {
 public:
-    int expand(int idx,vector<int>&nums)
-    {
-        int count = 0;
-        int idxcopy = idx;
-        idx++;
-        while(idx <nums.size() && nums[idx] !=0)
-        {
-            if(nums[idx] == 1)
-            count++;
-            idx++;
-        }
-        idx = idxcopy;
-        idx--;
-        while(idx>=0 && nums[idx]!=0)
-        {
-            if(nums[idx] == 1)
-            count++;
-            idx--;
-        }
-        return count;
-    }
+    map<int,int>mp;
     int longestSubarray(vector<int>& nums) {
         int length = 0;
-        for(int i =0;i<nums.size();i++) if(nums[i] == 0) length++;
-        if(length == 0) return nums.size()-1;
-        length = 0;
+        int j = 0;
         for(int i = 0;i<nums.size();i++)
         {
-            if(nums[i] == 0)
+            mp[nums[i]]++;
+            while(mp[0]>1)
             {
-                length = max(length,expand(i,nums));
+                mp[nums[j]]--;
+                j++;
             }
+            length = max(i-j+1,length);
         }
-        return length;
+        return length-1;
     }
 };
