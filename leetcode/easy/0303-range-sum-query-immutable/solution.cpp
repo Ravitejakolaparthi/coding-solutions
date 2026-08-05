2d639b0e8e3155f73f409de9e49ba37779d97906
Ravitejakolaparthi <ravitejakolaparthi97@gmail.com>
2026-08-06 00:35:29 +0530

    ✅ Range Sum Query - Immutable | Easy | C++ | Runtime: 0 ms (100.00%) | Memory: 23.9 MB | 2026-08-05

--- leetcode/easy/0303-range-sum-query-immutable/solution.cpp
+++ leetcode/easy/0303-range-sum-query-immutable/solution.cpp
@@ -1,16 +1,25 @@
 class NumArray {
 public:
-    vector<int>nums;
+    // vector<int>nums;
+    vector<int>p;
     NumArray(vector<int>& nums) {
-        this ->nums = nums;
+        // this ->nums = nums;
+        
+        p.resize(nums.size());
+        p[0]=nums[0];
+        for(int i = 1;i<nums.size();i++)
+        {
+            p[i] = p[i-1]+nums[i];
+        }
     }
     
     int sumRange(int left, int right) {
-        int sum=0;
-        for(int i = left;i<=right;i++)
-        sum+=nums[i];
-
-        return sum;
+        // int sum=0; 
+       
+        if(left > 0)
+        return p[right] - p[left-1];
+        else
+        return p[right];
     }
 };
 
