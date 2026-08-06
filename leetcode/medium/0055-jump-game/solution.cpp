d03bee3bc17a5180fc9c8644b7aa9450a50a5dfd
Ravitejakolaparthi <ravitejakolaparthi97@gmail.com>
2026-08-06 21:28:18 +0530

    ✅ Jump Game | Medium | C++ | Runtime: 0 ms | Memory: 8.3 MB | 2026-08-06

--- leetcode/medium/0055-jump-game/solution.cpp
+++ leetcode/medium/0055-jump-game/solution.cpp
@@ -3,8 +3,11 @@ public:
     bool canJump(vector<int>& nums) {
         if(nums.size() == 1)
         return true;
-        for(int i=0;i<nums.size();i++)
+        
+        for(int i=0;i<nums.size()-1;i++)
         {
+            if(nums[i] >= nums.size()-1)
+            return true;
             if(nums[i]<=0)
             return false;
         }
