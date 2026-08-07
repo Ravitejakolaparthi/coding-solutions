791432e5fcba8a1b8ff877a9ba98b91413e01e8a
Ravitejakolaparthi <ravitejakolaparthi97@gmail.com>
2026-08-07 21:50:55 +0530

    ✅ Product of Array Except Self | Medium | C++ | Runtime: 0 ms | Memory: 8.4 MB | 2026-08-07

---
+++ leetcode/medium/0238-product-of-array-except-self/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+            vector<int>prefix_product(nums.size());
+            prefix_product[0] = 1;
+            int right_product_to_i = 1;
+            for(int i = 1;i<=nums.size()-1;i++)
+                prefix_product[i] =prefix_product[i-1] * nums[i-1];
+            for(int i = prefix_product.size()-1;i>0;i--)
+            {
+                prefix_product[i] *=right_product_to_i;
+                right_product_to_i *= nums[i];
+            }
+            prefix_product[0]=right_product_to_i*1;
+            return prefix_product;
+
+    }
+};
