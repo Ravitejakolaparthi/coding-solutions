88725e1a87a715471c9fe0764cd4e608535e117d
Ravitejakolaparthi <ravitejakolaparthi97@gmail.com>
2026-08-08 11:44:02 +0530

    ✅ Jump Game II | Medium | C++ | Runtime: 0 ms | Memory: 8.2 MB | 2026-08-08

---
+++ leetcode/medium/0045-jump-game-ii/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int can_go = nums[0];
+        int Jumps = 0;
+        for(int i = 1;i<nums.size();i++){
+            if(can_go < nums.size()-1)
+            {
+                Jumps++;
+            }
+            can_go = max(can_go,i+nums[i]);
+        }
+        return Jumps;
+    }
+};
