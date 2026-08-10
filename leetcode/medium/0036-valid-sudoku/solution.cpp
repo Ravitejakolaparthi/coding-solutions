07e132c9450981b8a7e7406689620986b3b496e2
Ravitejakolaparthi <ravitejakolaparthi97@gmail.com>
2026-08-10 19:03:04 +0530

    ✅ Valid Sudoku | Medium | C++ | Runtime: 0 ms | Memory: 8.7 MB | 2026-08-10

--- leetcode/medium/0036-valid-sudoku/solution.cpp
+++ leetcode/medium/0036-valid-sudoku/solution.cpp
@@ -1,86 +1,67 @@
 class Solution {
 public:
-    bool checkrow(vector<vector<char>>&b,int i,int j){
-        unordered_set<char>s;
-             for(j = 0;j<9;j++)
-            {
-                if(b[i][j] == '.') continue;
-                if(s.find(b[i][j]) == s.end())
-                {
-                    s.insert(b[i][j]);
-                }
-                else{
-                    return false;
-                }
-            }
-            return true;
-    }
-    bool find(vector<vector<char>>&b,int i,int j)
-    {
-        unordered_set<int>s;
-        for(int k = i;k<i+3;k++)
-        {
-            for(int l = j;l<j+3;l++)
-            {
-                if(b[k][l] == '.') continue;
-                if(s.find(b[k][l]) == s.end())
-                {
-                    s.insert(b[k][l]);
-                }
-                else
-                {
-                    return false;
-                }
+    int n,m;
+    int find(int i){
+        if(i < 3) return 0;
+        if(i >= 3 && i < 6) return 3;
+        if(i >= 6) return 6;
+        return -1;
+    } 
+    bool checkrow(int i,vector<vector<char>>&b){
+        set<char>s;
+        for(int k = 0;k<n;k++){
+            if(b[i][k] == '.') continue;
+            if(s.count(b[i][k]) > 0)
+            return false;
+            else{
+                s.insert(b[i][k]);
             }
         }
         return true;
-
     }
-    bool checkbox(vector<vector<char>>&b,int i,int j) {
-           
-           int row = (i/3)*3;
-           int col = (j/3)*3;
-           return  find(b,row,col);
-           
-            
-    }
-    bool checkcol(vector<vector<char>>&b,int i,int j){
-            unordered_set<char>s;
-             for(i = 0;i<9;i++)
-            {
-                if(b[i][j] == '.') continue;
-                if(s.find(b[i][j]) == s.end())
-                {
-                    s.insert(b[i][j]);
-                }
-                else{
-                    return false;
-                }
-            }
-            return true;
-    }
-    bool solve(vector<vector<char>>&b,int i,int j)
-        {
-            return checkrow(b,i,j) && checkcol(b,i,j) && checkbox(b,i,j);
+    bool checkcol(int j,vector<vector<char>>&b){
+        set<char>s;
+        for(int k=0;k<m;k++){
+            if(b[k][j] == '.') continue;
+            if(s.count(b[k][j]) > 0)
+            return false;
+            else
+            s.insert(b[k][j]);
         }
-    bool isValidSudoku(vector<vector<char>>& board) {
-        
-        for(int i = 0;i<9;i++)
-        {
-            for(int j = 0;j<9;j++)
+        return true;
+    }
+    bool checkBox(int i,int j,vector<vector<char>>&b){
+        set<char>s;
+        int row = find(i);
+        int col = find(j);
+        for(int k = row;k<row+3;k++){
+            for(int l = col;l<col+3;l++)
             {
-                if(board[i][j] == '.')
-                {
-                   continue;
-                }
+                if(b[k][l] == '.') continue;
+                if(s.count(b[k][l]) > 0) 
+                return false;
                 else
-                {
-                     if(!solve(board,i,j)){
+                s.insert(b[k][l]);
+            }
+        }
+        return true;
+    }
+    bool solve(int i,int j,vector<vector<char>>&b){
+        if(checkrow(i,b) && checkcol(j,b) && checkBox(i,j,b))
+        return true;
+        return false;
+    }
+    bool isValidSudoku(vector<vector<char>>& board) {
+         n = board.size();
+         m = board[0].size();
+            for(int i = 0;i<n;i++){
+                for(int j = 0;j<m;j++){
+                    if(board[i][j] != '.'){
+                        if(!solve(i,j,board))
                         return false;
                     }
                 }
             }
-        }
-        return true;
+       return true;
     }
 };
