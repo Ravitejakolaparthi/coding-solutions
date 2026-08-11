da1bd47690cd8604768ad33f09cfd3ce427e00fe
Ravitejakolaparthi <ravitejakolaparthi97@gmail.com>
2026-08-11 17:39:22 +0530

    ✅ Magnetic Force Between Two Balls | Medium | C++ | Runtime: 0 ms | Memory: 8.2 MB | 2026-08-11

---
+++ leetcode/medium/1552-magnetic-force-between-two-balls/solution.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool check(long long int limit,long long int m,vector<int>&position){
+        m-=1; // we always put ball in first box
+        long long int start = position[0]; // we always start from first empty box
+        for(int i = 1;i<position.size();i++){
+            if(position[i] - start >= limit){
+                m-=1;
+                start = position[i];
+            }
+            if(m == 0)
+            return true;
+        }
+        return false;
+    }
+    int maxDistance(vector<int>& position, int m) {
+        sort(position.begin(),position.end());
+        long long int low = 1; // possible min distance
+        long long int high = position.back() - position.front(); // possible max distance between any two balls
+        while(low <= high){
+            long long int mid = low + (high-low)/2;
+            if(check(mid,m,position)){
+                low = mid+1;
+            }
+            else{
+                high = mid -1;
+            }
+        }
+        return high;
+    }
+};
