4f2056842bdb025cc79ba5587870eb06cb318e2e
Ravitejakolaparthi <ravitejakolaparthi97@gmail.com>
2026-08-14 14:58:47 +0530

    ✅ Maximum Candies Allocated to K Children | Medium | C++ | Runtime: 47 ms (9.99%) | Memory: 88.3 MB | 2026-08-14

---
+++ leetcode/medium/2226-maximum-candies-allocated-to-k-children/solution.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool check(long long limit,long long int k,vector<int>&candies){
+        long long int students = 0;
+        for(int i = 0;i<candies.size();i++){
+            students += candies[i]/limit;
+        }
+        return students >= k;
+    }
+    int maximumCandies(vector<int>& candies, long long k) {
+        long long int low = 1;
+        long long int high = 0;
+        for(int i =0;i<candies.size();i++){
+            high += candies[i];
+        }
+        while(low <= high){
+            long long int mid = low + (high-low)/2;
+            if(check(mid,k,candies)){
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid - 1;
+            }
+        }
+        return high;
+
+    }
+};
