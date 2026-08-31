ac0bf82c216396df87ebdf7cb97f9580534f75d8
Ravitejakolaparthi <ravitejakolaparthi97@gmail.com>
2026-08-31 20:55:17 +0530

    ✅ Binary Search | Easy | C | Runtime: 0 ms | Memory: 8.5 MB | 2026-08-31

--- leetcode/easy/0704-binary-search/solution.c
+++ leetcode/easy/0704-binary-search/solution.c
@@ -1,11 +1,19 @@
-int search(int* nums, int numsSize, int target)
- {
-   int i=0;
-    while(numsSize--)
-    {
-       if(target == nums[i]) 
-        return i;
-       i++;
+int search(int* nums, int numsSize, int target) {
+ int low=0,high=numsSize-1;
+
+    while(low<=high){
+        int mid=low+(high-low)/2;
+
+        if(target==nums[mid]){
+            return mid;
+        }
+        else if(target>nums[mid]){
+            low=mid+1;
+        }
+        else{
+            high=mid-1;
+        }
     }
-     return -1;
+
+    return -1;   
 }
