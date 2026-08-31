75ec9499d90ea33818120136aa2504891e79add3
Ravitejakolaparthi <ravitejakolaparthi97@gmail.com>
2026-08-31 20:59:18 +0530

    ✅ Number of Islands | Medium | C++ | Runtime: 60 ms (6.70%) | Memory: 16.7 MB | 2026-08-31

--- leetcode/medium/0200-number-of-islands/solution.cpp
+++ leetcode/medium/0200-number-of-islands/solution.cpp
@@ -6,22 +6,21 @@ public:
         if(i >=0 && i <= n-1 && j >=0 && j <= m-1)
         {
             
-            if(v[i][j] == true)
-            return ;
-            if(grid[i][j] == '0')
-            return ;
-            else
+            if(v[i][j] != true)
             {
-                v[i][j] =true;
-                dfs(grid,i-1,j,n,m,v);
+             if(grid[i][j] != '0')
+             {
+                 v[i][j] = true;
+                 cout << i << j<<endl;
+
                 dfs(grid,i,j+1,n,m,v);
                 dfs(grid,i+1,j,n,m,v);
                 dfs(grid,i,j-1,n,m,v);
-              
+                dfs(grid,i-1,j,n,m,v);
             }
-
+            }
         }
-        // return;
+     
     }
     int numIslands(vector<vector<char>>& grid) {
         int n = grid.size();
